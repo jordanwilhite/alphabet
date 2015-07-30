d1b18ce08921787461308df4a3114ba84486e6b4
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-30 07:56:15 -0500

    backbone assignment with partial backbone

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+$(function(){
+  // Model
+
+          var Morph = Backbone.Model.extend ({
+            initialize: function(){
+              alert("Hooray! You morphed the text!");
+            }
+});
+
+          var caps = new Caps ();
+          var lower = new Low ();
+          var script = new Sript ();
+
+      // I know I need something else here to call or define caps.set, but don't know what that is"
+
+          caps.set([lowercase: 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+          s', 't', 'u', 'v', 'w', 'x' 'y' 'z], [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z])
+
+
+
+          var MorphType = Backbone.Collection.extend({
+});
+          var NewText = Backbone.View.extend({
+});
+          var App = Backbone.View.extend({
+          }
+
+    new App();
+})
